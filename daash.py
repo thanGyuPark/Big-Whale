7d8fce26af7bdf9bbfc79b2db4781f7b74b647b1
thanGyuPark <108649860+thanGyuPark@users.noreply.github.com>
2025-12-09 14:23:54 +0900

    Install required packages for daash.py

--- daash.py
+++ daash.py
@@ -1,3 +1,5 @@
+import subprocess, sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "yfinance pandas-ta vectorbt plotly --quiet"])
 # -*- coding: utf-8 -*-
 import streamlit as st
 import yfinance as yf
